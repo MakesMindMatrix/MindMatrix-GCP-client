32308226b2f65212f770d5579620b360155b2508
Nimish Kumar <175190568+MindMatrix1@users.noreply.github.com>
2025-05-02 17:18:31 +0530

    Upcoming card style

--- src/component/Home/Cards/UpcomingCard.js
+++ src/component/Home/Cards/UpcomingCard.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './UpcomingCard.css'
 
-const UpcomingCard = ({ data, setConfirmModal, setEnrollCourseData, enrollCourseData }) => {
-    
+const UpcomingCard = ({ data }) => {
+
     return (
         <>
             <div
                 // to={course_page_url}
                 // style={{ textDecoration: "none" }}
                 className='course_card courseCard_container'
-                style={{marginBottom: '2rem'}}
+                style={{ marginBottom: '2rem' }}
             >
                 <div
                     style={{
-                        backgroundImage: `url(${data?.url})`,height: "20rem"
+                        backgroundImage: `url(${data?.url})`, height: "20rem"
                     }}
                     className='course_card_img'
                 ></div>
-                <h1 className="course_name">{data?.course_name}</h1>
-                <div className='enroll_button' style={{textAlign: 'center'}}>Launching Soon!</div>
+                <div className='courseContent_box'>
+                    <h1 className="course_name">{data?.course_name}</h1>
+                    <div className='enroll_button' style={{ textAlign: 'center' }}>Launching Soon!</div>
+                </div>
             </div>
         </>
     )
