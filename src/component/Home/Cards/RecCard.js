8440dd6c1fd9de12a3771d7ebe1fd2c78d079e80
MindMatrix-Shril <shril@clinf.com>
2025-05-08 10:28:39 +0530

    fix grammar on landing, batch_price in rec courses

--- src/component/Home/Cards/RecCard.js
+++ src/component/Home/Cards/RecCard.js
@@ -62,7 +62,7 @@ const RecCard = ({ data}) => {
                     </div>
 
                     <div className='coursePrice_box'>
-                        <h2>₹50</h2>
+                        <h2>Rs. {data?.batch_price}</h2>
                         {/* {data.external_batch_id ? <button className='enroll_button' onClick={handleEnroll}>Enroll now</button> : null} */}
                     </div>
                 </div>
