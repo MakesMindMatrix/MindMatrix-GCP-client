2a16031b1af3d5ff0be754428726dc3f047f8389
Nimish Kumar <175190568+MindMatrix1@users.noreply.github.com>
2025-06-16 17:19:53 +0530

    download removed

--- src/component/Home/Cards/NoticeBoardCard.js
+++ src/component/Home/Cards/NoticeBoardCard.js
@@ -7,21 +7,17 @@ import eligibiltyBullet from '../images/eligibility_bullets_icon.svg'
 import view from '../images/view_icon.svg'
 import download from '../images/download.svg'
 
-const NoticeBoardCard = ({ data }) => { 
-  
+const NoticeBoardCard = ({ data }) => {
+
 
   const handleView = (fileId) => {
-    const viewUrl = `https://drive.google.com/file/d/${fileId}/preview`;
+    const viewUrl = `https://drive.google.com/file/d/${fileId}/view`;
     window.open(viewUrl, '_blank');
   };
 
   const handleDownload = (fileId) => {
     const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
-    console.log(downloadUrl)
-    const link = document.createElement('a');
-    link.href = downloadUrl;
-    link.download = 'document.pdf'; // optional: set a default file name
-    link.click();
+      window.open(downloadUrl, '_blank');
   };
 
   // console.log(data)
@@ -49,11 +45,11 @@ const NoticeBoardCard = ({ data }) => {
             <span><img src={attachmentBullet} alt="attachment-bullet" /> {att.title}</span>
             <div>
               <Link onClick={() => handleView(att.url)} target="_blank" className="noticeboard-view-btn">View<img src={view} alt="View" /></Link>
-              <Link onClick={() => handleDownload(att.url)} download target="_blank" className="noticeboard-download-btn">Download<img src={download} alt="Download" /></Link>
+              {/* <Link onClick={() => handleDownload(att.url)} download target="_blank" className="noticeboard-download-btn">Download<img src={download} alt="Download" rel="noopener noreferrer"/></Link> */}
             </div>
           </div>
         ))}
-        <div className="notice-dates-line"></div>
+        {data.attachments && data.attachments.length > 0 && <div className="notice-dates-line"></div>}
       </div>
 
       <div className="noticeboard-section notice-academic">
